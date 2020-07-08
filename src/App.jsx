bc621e1196bb42addb19d2321c6bba505516351c
陈晓斌 <465120558@qq.com>
2020-07-08 22:56:56 +0800

    使用Ref Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,48 @@
-import React, { Component, useState, useMemo, memo, useCallback } from 'react';
+import React, { Component, PureComponent, useState, useMemo, memo, useCallback, useEffect, useRef } from 'react';
 import './App.css';
 
-const Counter = memo(function Counter (props) {
-  console.log('Counter render1')
-  return (<div>{props.count}</div>)
-})
+// const Counter = memo(function Counter (props) {
+//   console.log('Counter render1')
+//   return (<div onClick={props.onClick}>{props.count}</div>)
+// })
 
-function App2 (props) {
+class Counter extends PureComponent {
+  speak () {
+    console.log(`now counter is: ${this.props.count}`)
+  }
+  render () {
+    const { props } = this;
+    return (<div onClick={props.onClick}>{props.count}</div>)
+  }
+}
 
+function App (props) {
   const [count, setCount] = useState(0);
+  const [clickCount, setClickCount] = useState(0);
+  const counterRef = useRef();
+  const it = useRef();
 
   const double = useMemo(() => {
     return count * 2;
   }, [count === 3]);
 
-  // const onClick = useMemo(() => {
-  //   return () => {
-  //     console.log('click')
-  //   };
-  // }, []);
- 
   const onClick = useCallback(() => {
-    console.log('click')
+    console.log('click');
+    setClickCount((clickCount) => clickCount + 1);
+    counterRef.current.speak()
+  }, [counterRef])
+
+  useEffect(() => {
+    it.current = setInterval(() => {
+      setCount(count => count + 1)
+    }, 1000)
   }, [])
+
+  useEffect(() => {
+    if (count >= 10) {
+      clearInterval(it.current);
+    }
+  })
 
   return (
     <div>
@@ -32,11 +52,13 @@ function App2 (props) {
       >
         Click({count}),double:({double})
       </button>
-      <Counter count={double}
+      <Counter
+        ref={counterRef}
+        count={double}
         onClick={onClick}
       />
     </div>
   )
 }
 
-export default App2; 
+export default App; 
