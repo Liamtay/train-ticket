cefd686493070dff1d6d379770d92a547e8c184b
陈晓斌 <465120558@qq.com>
2020-07-08 18:46:14 +0800

    使用Context Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,18 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { Component, useState, createContext, useContext } from 'react';
 import './App.css';
 
+const CountContext = createContext()
+
+function Counter () {
+
+  const count = useContext(CountContext)
+
+  return (<div>{count}</div>)
+}
 
 function App2 () {
 
   const [count, setCount] = useState(0);
-  const [size, setSize] = useState({
-    width: document.documentElement.clientWidth,
-    height: document.documentElement.clientHeight
-  })
-
-  useEffect(() => {
-    document.title = count
-  })
-
-  const onResize = () => {
-    setSize({
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight
-    })
-  }
-
-  useEffect(() => {
-    window.addEventListener('resize', onResize, false);
-
-    return () => {
-      window.removeEventListener('resize', onResize, false);
-    }
-  }, [])
-
-  const onClick = () => {
-    console.log('onclick');
-
-  }
-
-  useEffect(() => {
-    document.getElementById('size').addEventListener('click', onClick, false)
-
-    return ()=>{
-      document.getElementById('size').removeEventListener('click', onClick, false)
-    }
-  })
-
 
   return (
     <div>
@@ -50,31 +21,13 @@ function App2 () {
         onClick={() => { setCount(count + 1) }}
       >
         Click({count})
-        </button>
-      {count & 1 == 1 ?
-        <span id='size'>size:{size.width}x{size.height}</span>
-        : <p id='size'>size:{size.width}x{size.height}</p>
-      }
+    </button>
+      <CountContext.Provider value={count}>
+        <Counter />
+      </CountContext.Provider>
     </div>
   )
 }
 
-function App (props) {
-
-  const [count, setCount] = useState(() => {
-    return props.defaultCount || 0;
-  });
-
-  return (
-    <div>
-      <button
-        type='button'
-        onClick={() => { setCount(count + 2) }}
-      >
-        Click({count})
-      </button>
-    </div>
-  )
-}
 
 export default App2; 
