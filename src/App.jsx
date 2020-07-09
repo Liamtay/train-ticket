a1b2f46d145b69aa70b9b34974d50b134100d543
陈晓斌 <465120558@qq.com>
2020-07-09 19:51:59 +0800

    3-1Context实现跨层级的组件数据传递

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,58 @@
-import React, { Component, PureComponent, useState, useMemo, memo, useCallback, useEffect, useRef } from 'react';
+import React, { Component, createContext } from 'react';
 import './App.css';
 
-// const Counter = memo(function Counter (props) {
-//   console.log('Counter render1')
-//   return (<div onClick={props.onClick}>{props.count}</div>)
-// })
+const BatteryContext = createContext();
+const OnlineContext = createContext();
 
-class Counter extends PureComponent {
-  speak () {
-    console.log(`now counter is: ${this.props.count}`)
+class Leaf extends Component {
+  render () {
+    return (
+      <BatteryContext.Consumer>
+        {
+          (battery) => (
+            <OnlineContext.Consumer>
+              {
+                online => <h1>Battery:{battery} , Online:{String(online)}</h1>
+              }
+            </OnlineContext.Consumer>
+          )
+        }
+      </BatteryContext.Consumer>
+    )
+  }
+}
+
+class Middle extends Component {
+  render () {
+    return <Leaf />;
+  }
+}
+
+class App extends Component {
+  state = {
+    online: false,
+    battery: 60
   }
   render () {
-    const { props } = this;
-    return (<div onClick={props.onClick}>{props.count}</div>)
+    const { battery, online } = this.state;
+    return (
+      <BatteryContext.Provider value={battery}>
+        <OnlineContext.Provider value={online}>
+          <button
+            type="button"
+            onClick={() => this.setState({ battery: battery - 1 })}>
+            Press
+          </button>
+          <button
+            type="button"
+            onClick={() => this.setState({ online: !online })}>
+            Switch
+          </button>
+          <Middle />
+        </OnlineContext.Provider>
+      </BatteryContext.Provider>
+    );
   }
-}
-
-function App (props) {
-  const [count, setCount] = useState(0);
-  const [clickCount, setClickCount] = useState(0);
-  const counterRef = useRef();
-  const it = useRef();
-
-  const double = useMemo(() => {
-    return count * 2;
-  }, [count === 3]);
-
-  const onClick = useCallback(() => {
-    console.log('click');
-    setClickCount((clickCount) => clickCount + 1);
-    counterRef.current.speak()
-  }, [counterRef])
-
-  useEffect(() => {
-    it.current = setInterval(() => {
-      setCount(count => count + 1)
-    }, 1000)
-  }, [])
-
-  useEffect(() => {
-    if (count >= 10) {
-      clearInterval(it.current);
-    }
-  })
-
-  return (
-    <div>
-      <button
-        type='button'
-        onClick={() => { setCount(count + 1) }}
-      >
-        Click({count}),double:({double})
-      </button>
-      <Counter
-        ref={counterRef}
-        count={double}
-        onClick={onClick}
-      />
-    </div>
-  )
-}
+} 
 
 export default App; 
